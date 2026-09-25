495a44fb84a900069f81dcc3eb5b99a16dc10548
sakib hasan <mdsakibhasanankon@gmail.com>
2026-09-25 18:46:24 +0600

    Created my plan page/route and link through all the components in navbar like my plan, plan and saved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,14 +22,22 @@ const Navbar = () => {
             <h2 className="text-[#C2F800] bg-[#C2F800]/10 py-1 text-center px-4 rounded-3xl cursor-pointer ">Workouts</h2>
             </Link>
 
+            <Link href={'/my-plan'}>
             <h2 className="text-[#9CA3AF] cursor-pointer">My Plan</h2>
+            </Link>
         </div>
         {/* plan and saved section  */}
         <div className="flex gap-7 ">
+
+          <Link href={'/my-plan'}>
             <h3 className="text-[#D1D5DB] cursor-pointer">Plan <span className="inline-flex w-6 h-6  items-center justify-center rounded-full bg-[#C2F800] text-black font-bold "> 0 </span>
             </h3>
+            </Link>
+
+            <Link href={'/my-plan'}>
             <h3 className="text-[#9CA3AF] cursor-pointer">Saved <span className="inline-flex w-6 h-6  items-center justify-center rounded-full bg-[#2D313B] text-[#D1D5DB] font-bold "> 0 </span>
             </h3>
+            </Link>
         </div>
       </nav>
     </div>
